b3dae3926eda67e95e59888ed24e768ac6db96a1
Edson Martin Feitosa <edsonmfeitosa@hotmail.com>
2026-03-02 21:16:31 -0300

    Exemplo de entrada de usuário no javascript

---
+++ Javascript/Aula02/EntradaUsuario/entrada.js
@@ -0,0 +1,3 @@
+let nome = prompt("Digite seu nome: ")
+console.log("Olá, " + nome + "! Bem-vindo a disciplina de linguagens de programação!")
+alert("Olá, " + nome + "! Bem-vindo a disciplina de Linguagens de Programação!")
